8726e24c4c8e1b32d6dd6db07bf89c8a244849cc
sandhyasantosh <sandhyasantosh98@outlook.com>
2022-06-07 19:13:11 +0530

    Modified forecast controller

--- AzureHandson4/Controllers/WeatherForecastController.cs
+++ AzureHandson4/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace AzureHandson4.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Haze"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
@@ -27,7 +27,7 @@ namespace AzureHandson4.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 10).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
